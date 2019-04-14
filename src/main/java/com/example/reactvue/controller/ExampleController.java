a269be9d179931c4fc224d02bb03e2fdb035c006
Collado <collado_ztz@hotmail.com>
2019-04-14 21:08:42 +0800

    init

---
+++ src/main/java/com/example/reactvue/controller/ExampleController.java
@@ -0,0 +1,9 @@
+package com.example.reactvue.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/example")
+public class ExampleController {
+}
