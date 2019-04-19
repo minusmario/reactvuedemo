ab1fcca4158d4d53e071e37e7031a6cbc5fe72c4
zhangtz <zhangtz@bosera.com>
2019-04-19 15:06:17 +0800

    Vue异步请求&列表渲染

--- src/main/js/vuedemo/src/store.js
+++ src/main/js/vuedemo/src/store.js
@@ -1,16 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     userInput: '',
+    listData: [],
   },
   mutations: {
-    setUserInput(state, payload){
+    setUserInput(state, payload) {
       state.userInput = payload.textValue;
-    }
+    },
+    takeListData(state, payload) {
+      state.listData = payload.listData;
+    },
   },
-  actions: {},
+  actions: {
+    getListData({ commit }) {
+      axios.get('/example').then(function (res) {
+        commit('takeListData', { listData: res.data });
+      });
+    },
+  },
 });
--- src/main/js/vuedemo/src/views/About.vue
+++ src/main/js/vuedemo/src/views/About.vue
@@ -5,6 +5,12 @@
         <input id='input' type='text' v-model='userInput'>
         <p>您输入的字符依次为：{{userInput}}</p>
         <button type='button' @click='updateUserInput()'>点击此处将输入框文本替换为预置文本</button>
+        <button type='button' @click='getListData()'>点击此处获取列表数据</button>
+        <ul>
+            <li v-for="data in listData" :key="data.row">
+                <span>数据编号为：{{data.row}}</span><br><span>数据内容为：{{data.data}}</span>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -26,10 +32,18 @@
           this.$store.commit('setUserInput', { textValue: value });
         },
       },
+      listData: {
+        get: function () {
+          return this.$store.state.listData;
+        },
+      },
     },
     methods: {
       updateUserInput() {
         this.$store.commit('setUserInput', { textValue: this.preset });
+      },
+      getListData() {
+        this.$store.dispatch('getListData');
       },
     },
   };
